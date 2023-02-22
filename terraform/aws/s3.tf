85bbcfddf19e70c8dff43a1ad9814c7a1d0c54bd
koheifukushima <kfukushima@paloaltonetworks.com>
2023-02-21 23:12:57 -0800

    this change is test2

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -32,7 +32,7 @@ resource "aws_s3_bucket_object" "data_object" {
     }, {
     git_commit           = "d68d2897add9bc2203a5ed0632a5cdd8ff8cefb0"
     git_file             = "terraform/aws/s3.tf"
-    git_last_modified_at = "2020-06-16 s4:46:24"
+    git_last_modified_at = "2020-06-16 4:46:24"
     git_last_modified_by = "nimrodkor@gmail.com"
     git_modifiers        = "nimrodkor"
     git_org              = "bridgecrewio"
